067afb5c438e09f4819cb5309aa5d94f7797ce23
Paul V. Hoffman <pvhoffman@gmail.com>
2015-02-03 14:16:08 -0800

    plot1.R and plot1.png finished

--- plot1.R
+++ plot1.R
@@ -1,16 +1,21 @@
 data.in.filename <- function() {
         "household_power_consumption.txt"
 }
-
+line.number.for.date <- function(date){
+        ls <- grep(date, readLines(data.in.filename()))
+        ls[1] - 1;
+}
 read.power.consumption.data <- function() {
-        inp <- fread(data.in.filename(), colClasses="character")
-        setnames(inp, c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-        inp$Date <- as.Date(inp$Date, format="%d/%m/%Y")
-
-        ts <- inp[inp$Date=="2007-02-01" | inp$Date=="2007-02-02"]
-        ts <- data.frame(ts)
-        ts[,3] <- as.numeric(ts[,3])
-        ts
+        ln1 <- 66637 # line.number.for.date("1/2/2007")
+        ln2 <- 68077 # line.number.for.date("2/2/2007")
+        t1 <- read.table(data.in.filename(), header=FALSE, sep=";", skip=ln1, nrows=1440, colClasses="character", na.strings="?")
+        t2 <- read.table(data.in.filename(), header=FALSE, sep=";", skip=ln2, nrows=1440, colClasses="character", na.strings="?")
+        t3 <- rbind(t1, t2)
+        names(t3) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+        t3$Date <- as.Date(t3$Date, format="%d/%m/%Y")
+        t3 <- data.frame(t3)
+        t3[,3] <- as.numeric(t3[,3])
+        t3
 }
 
 din <- read.power.consumption.data()
